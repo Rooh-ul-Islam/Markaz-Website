e6e1153c64ae51db6ae8d118e90c8fce8edbc1ba
Muhammad Hassaan Shaukat <126575945+muhammad-hassaan-y2@users.noreply.github.com>
2024-11-17 20:39:56 +0500

    Update Mission.tsx

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -3,24 +3,17 @@ import React from 'react'
 const Mission = () => {
   return (
     <div className="bg-gray-100 text-gray-800 py-16 px-6 sm:px-12">
-  <div className="max-w-7xl mx-auto text-center">
-    <h2 className="text-5xl font-bold text-cyan-800 mb-6 underline decoration-cyan-600">
-      Our Mission
-    </h2>
-    <p className="text-lg md:text-xl mb-6">
-      At Markaz Rooh-ul-Islam Jamia, we are committed to advancing education, 
-      serving the community, and spreading the teachings of Islam. Our institution
-      has been helping students achieve academic excellence while also providing them 
-      with a space for spiritual growth and development.
-    </p>
-    <p className="text-lg md:text-xl mb-6">
-      Through dedication, teamwork, and reliance on Allah, we aim to create a positive 
-      impact on society by offering quality education, fostering moral values, and preparing 
-      students to contribute meaningfully to the community and the world.
-    </p>
-  </div>
-</div>
-
+      <div className="max-w-7xl mx-auto text-center">
+        <h2 className="text-5xl font-bold text-cyan-800 mb-6 underline decoration-cyan-600">
+          Our Goal
+        </h2>
+        <ul className="text-lg md:text-xl mb-6 list-inside list-decimal">
+          <li>To make righteous Muslims by providing religious education and training.</li>
+          <li>To provide opportunities for modern sciences along with religion to scholars.</li>
+          <li>To make citizens with skills and character.</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
